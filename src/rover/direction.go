c55e6bbd1ec591b24efd73324a4fce96b38a64c2
mattes <matthias.kadenbach@gmail.com>
2013-04-16 18:49:21 +0200

    tests for direction

--- src/rover/direction.go
+++ src/rover/direction.go
@@ -4,3 +4,8 @@ type direction struct {
   Point point
   Degree int
 }
+
+func (d *direction) Rotate(degree int) *direction {
+  d.Degree = degree % 360
+  return d // keep it chainable
+}
--- src/rover/rover.go
+++ src/rover/rover.go
@@ -1,8 +1,8 @@
 package rover
 
 
-func Move(x int) point {
+func Move(x int) int {
   p := direction{point{1, 2}, 90}
-  p.Point.X = 2
-  return p.Point
+  p.Point.X = 4
+  return p.Point.X
 }
