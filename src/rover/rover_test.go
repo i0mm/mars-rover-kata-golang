22b45b618a6331977f9a61fd0fe00796dee12fcb
mattes <matthias.kadenbach@gmail.com>
2013-04-18 10:49:33 +0200

    code cleanup

--- src/rover/rover_test.go
+++ src/rover/rover_test.go
@@ -14,19 +14,3 @@ func TestCommandString(t *testing.T) {
 
   
 }
-
-
-/*
-func TestForward(t *testing.T) {
-  r := Rover{}
-
-  r.Place(0, 0, 90)
-  if(len(r.Path) != 1) {
-    t.Errorf("rover has 1 path item")
-  }
-
-  if r.Forward(); len(r.Path) != 3 && r.CurrentPosition().Point.X != 99 {
-    t.Errorf("expected , got %v", r.CurrentPosition())
-  }
-}
-*/
